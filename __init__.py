c3c246853210735ad2f1ce3d1dd06c01bcc4ff0f
pggPL <pawelgadzinski@gmail.com>
2021-01-13 15:07:41 +0100

    Implemented basic objects

---
+++ __init__.py
@@ -0,0 +1,61 @@
+localization = "pytex/"
+
+# Main file of PyTeX package
+
+class Pytex():
+	##############
+	##   Code   ##
+	##############
+
+	package_init = ""
+	head_code = ""
+	document_code = ""
+
+
+	#################
+	##   Packages  ##
+	#################
+
+	packages_list = []
+
+	def init():
+		packages_list_file = open(localization + "packages/packages_list", "r")
+		Pytex.packages_list = packages_list_file.read().split("\n")
+	
+	def execute():
+		output_code = ""
+		output_code += "\document_class{" + document.document_class + "}\n \n"
+		output_code += Pytex.package_init
+		output_code += "\n \n"
+		output_code += Pytex.head_code
+		output_code += "\\begin{document}\n"
+		output_code += Pytex.document_code
+		output_code += "\\end{document}\n"
+		print(output_code)
+
+
+
+def include(package_name):
+	if package_name in Pytex.packages_list:
+		package_file = open(localization + "packages/" + package_name, "r")
+		Pytex.package_init += package_file.read()
+	else:
+		Pytex.package_init += "\include{" + package_name + "}\n"
+
+def tex(x):
+	Pytex.document_code += x + "\n";
+
+def execute():
+	Pytex.execute()
+
+class document():
+	####################
+	##   Properties   ##
+	####################
+	document_class = "article"
+	title = ""
+	author = ""
+	date = ""
+
+def make_title():
+	tex("\maketitle")
